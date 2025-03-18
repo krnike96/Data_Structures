0ef8c4be55a3b68f8b0b90fa1c667a6daff7d513
Niket Kumar <krnike96@gmail.com>
2025-03-18 12:09:39 +0530

    Display and Element Count Implemented! All Stack operations Done

--- Stack/Stack_Array.c
+++ Stack/Stack_Array.c
@@ -1,6 +1,6 @@
 #include "stdio.h"
 #include "stdlib.h"
-
+int count = 0;
 typedef struct stackArray{
     int * arr;
     int data;
@@ -15,15 +15,34 @@ int isempty(stack *s){
 }
 int push(stack *s,int size,int data){
     if(isfull(s,size)) return -9999999;
-    else return s->arr[++s->top] = data;
+    else {
+        count++;
+        return s->arr[++s->top] = data;
+    }
 }
 int pop(stack *s){
     if(isempty(s)) return -9999999;
-    else return s->arr[s->top--];
+    else {
+        count--;
+        return s->arr[s->top--];
+    }
 }
-int peek(stack);
+int peek(stack *s){
+    if(isempty(s)) return -9999999;
+    else return s->arr[s->top];
+}
 
-void display(stack);
+void display(stack *s){
+    if(isempty(s)){
+        printf("\nThe Stack is Empty!");
+        return;
+    }
+    printf("\nThe Stack is : ");
+    for(int i = 0;i <= s->top;i++){
+        printf("%d ",s->arr[i]);
+    }
+    printf("\nNo. of Elements : %d",count);
+}
 
 int main(){
     stack *s = (stack *)malloc(sizeof(stack));
@@ -37,12 +56,14 @@ int main(){
     }
     s->arr = (int *)malloc(sizeof(int)*size);
     int choice,data;
+    printf("\nStack using Array");
     while(1){
         printf("\n1.Push");
         printf("\n2.Pop");
         printf("\n3.Peek");
         printf("\n4.Check Fullness");
         printf("\n5.Check Emptiness");
+        printf("\n6.Display");
         printf("\n0.Exit");
         printf("\nEnter your choice : ");
         scanf("%d",&choice);
@@ -60,7 +81,10 @@ int main(){
                 else printf("\nThe Popped Element is : %d",data);
                 break;
             case 3:
-
+                data = peek(s);
+                if(data == -9999999) printf("\nCan't Peek. The Stack is Empty!");
+                else printf("\nThe Element at the Top is : %d",data);
+                break;
             case 4:
                 if(isfull(s,size)) printf("\nThe Stack is Full.");
                 else printf("The Stack is not Full.");
@@ -69,10 +93,15 @@ int main(){
                 if(isempty(s)) printf("\nThe Stack is Empty!");
                 else printf("\nThe Stack is not Empty!");
                 break;
+            case 6:
+                display(s);
+                break;
             case 0:
                 printf("\nExiting...");
                 free(s->arr);
+                s->arr = NULL;
                 free(s);
+                s = NULL;
                 return 0;
             default:
                 printf("\nError 400 : Bad Behaviour");
